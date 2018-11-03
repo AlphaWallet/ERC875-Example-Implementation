9598b72695d9543169a31d449673c9a02e9f8ef9
James Sangalli <j.l.sangalli@gmail.com>
2018-11-03 19:33:11 +0100

    add ERC165

--- ERCTokenImplementation.sol
+++ ERCTokenImplementation.sol
@@ -1,4 +1,15 @@
-contract ERC
+contract ERC165 
+{
+            /// @notice Query if a contract implements an interface
+            /// @param interfaceID The interface identifier, as specified in ERC-165
+            /// @dev Interface identification is specified in ERC-165. This function
+            ///  uses less than 30,000 gas.
+            /// @return `true` if the contract implements `interfaceID` and
+            ///  `interfaceID` is not 0xffffffff, `false` otherwise
+            function supportsInterface(bytes4 interfaceID) external view returns (bool);
+}
+
+contract ERC875 /* is ERC165 */
 {
   event Transfer(address indexed _from, address indexed _to, uint256[] tokenIndices);
 
@@ -15,7 +26,7 @@ contract ERC
 }
 
 pragma solidity ^0.4.17;
-contract Token is ERC
+contract Token is ERC875
 {
     uint totalTickets;
     mapping(address => uint256[]) inventory;
